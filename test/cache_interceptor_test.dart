bb68b1ac173b099ca5bd0207f26dac1f11d02f89
llfbandit <llfbandit@gmail.com>
2021-03-05 18:45:58 +0100

    core: Add test_coverage

--- test/cache_interceptor_test.dart
+++ test/cache_interceptor_test.dart
@@ -1,5 +1,8 @@
+import 'dart:io';
+
 import 'package:dio/dio.dart';
 import 'package:dio_cache_interceptor/dio_cache_interceptor.dart';
+import 'package:dio_cache_interceptor/src/store/file_cache_store.dart';
 import 'package:test/test.dart';
 
 import 'mock_adapter.dart';
@@ -12,7 +15,8 @@ void main() {
   setUp(() async {
     _dio = Dio()..httpClientAdapter = MockAdapter();
 
-    store = MemCacheStore();
+    store = FileCacheStore('${Directory.current.path}/test/data');
+    await store.clean();
     options = CacheOptions(store: store);
 
     _dio.interceptors.add(
@@ -20,7 +24,7 @@ void main() {
     );
   });
 
-  tearDown(() {
+  tearDown(() async {
     _dio.close();
   });
 
@@ -110,7 +114,6 @@ void main() {
       );
     } catch (err) {
       expect((err as DioError).response?.statusCode, equals(500));
-      return;
     }
 
     try {
