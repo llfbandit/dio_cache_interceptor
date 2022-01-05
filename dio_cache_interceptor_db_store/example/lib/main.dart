d6652e96587eb7532aede0779a563fbdb268cd85
llfbandit <llfbandit@gmail.com>
2022-01-05 20:42:50 +0100

    core: Add foo examples to store packages for pub.dev

---
+++ dio_cache_interceptor_db_store/example/lib/main.dart
@@ -0,0 +1,20 @@
+void main(List<String> arguments) {
+  // Full example is available at
+  // https://github.com/llfbandit/dio_cache_interceptor/blob/master/dio_cache_interceptor/example/lib/main.dart
+
+  // late CacheStore cacheStore;
+
+  // pp.getTemporaryDirectory().then((dir) {
+  //   cacheStore = DbCacheStore(databasePath: dir.path, logStatements: true);
+
+  //   cacheOptions = CacheOptions(
+  //     store: cacheStore,
+  //     hitCacheOnErrorExcept: [], // for offline behaviour
+  //   );
+
+  //   dio = Dio()
+  //     ..interceptors.add(
+  //       DioCacheInterceptor(options: cacheOptions),
+  //     );
+  // });
+}
