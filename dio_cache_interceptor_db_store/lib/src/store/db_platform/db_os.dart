9dd8712d9443c8ad3466c9116dc6736854a8a81a
dragon <kenta3ds.habbo@gmail.com>
2022-11-29 22:16:29 +0100

    fix: Correctly close Isolate database (#97)

--- dio_cache_interceptor_db_store/lib/src/store/db_platform/db_os.dart
+++ dio_cache_interceptor_db_store/lib/src/store/db_platform/db_os.dart
@@ -1,8 +1,4 @@
 import 'dart:io';
-import 'dart:isolate';
-
-import 'package:drift/drift.dart';
-import 'package:drift/isolate.dart';
 import 'package:drift/native.dart';
 import 'package:path/path.dart' as p;
 
@@ -14,39 +10,9 @@ DioCacheDatabase openDb({
   bool logStatements = false,
 }) {
   Directory(databasePath).createSync(recursive: true);
-  final dbFullPath = p.join(databasePath, '$databaseName.db');
+  final dbFile = File(p.join(databasePath, '$databaseName.db'));
 
   return DioCacheDatabase.connect(
-    DatabaseConnection.delayed(
-      Future(() async {
-        final isolate = await _createDriftIsolate(dbFullPath);
-        return isolate.connect();
-      }),
-    ),
+    NativeDatabase.createBackgroundConnection(dbFile),
   );
-}
-
-Future<DriftIsolate> _createDriftIsolate(String path) async {
-  final receivePort = ReceivePort();
-  await Isolate.spawn(
-    _startBackground,
-    _DriftIsolateData(receivePort.sendPort, path),
-  );
-
-  return await receivePort.first as DriftIsolate;
-}
-
-void _startBackground(_DriftIsolateData data) {
-  final executor = NativeDatabase(File(data.path));
-  final driftIsolate = DriftIsolate.inCurrent(
-    () => DatabaseConnection(executor),
-  );
-  data.port.send(driftIsolate);
-}
-
-class _DriftIsolateData {
-  _DriftIsolateData(this.port, this.path);
-
-  final SendPort port;
-  final String path;
 }
