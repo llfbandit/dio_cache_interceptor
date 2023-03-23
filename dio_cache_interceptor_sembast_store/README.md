e46daf22561c2786d4267d7968a6a9e5f0ddc9f1
unknown <murattinhokurban@gmail.com>
2023-03-23 16:01:53 +0500

    added sembast database support for dio cache interceptor

---
+++ dio_cache_interceptor_sembast_store/README.md
@@ -0,0 +1,3 @@
+## dio_cache_interceptor_sembast_store
+
+Sembast cache store implementation.
---
+++ dio_cache_interceptor_sembast_store/lib/dio_cache_interceptor_sembast_store.dart
@@ -0,0 +1,3 @@
+library dio_cache_interceptor_sembast_store;
+
+export 'src/store/dio_cache_interceptor_sembast_store.dart';
